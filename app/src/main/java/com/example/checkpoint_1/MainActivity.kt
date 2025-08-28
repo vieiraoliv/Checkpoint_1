b6c4cd97a93fe406e57056f9dd850f63ad70a69d
Henrique Oliveira <riqueoliveira1910@gmail.com>
2025-08-28 11:50:02 -0300

    Initial commit

---
+++ app/src/main/java/com/example/checkpoint_1/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.checkpoint_1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.checkpoint_1.ui.theme.Checkpoint_1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Checkpoint_1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Text(text = "Hello World",modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
