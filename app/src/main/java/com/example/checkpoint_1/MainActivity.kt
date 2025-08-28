a6d613cf0451a1175f23e7cfd259a25bd8c24118
Henrique Oliveira <riqueoliveira1910@gmail.com>
2025-08-28 12:57:30 -0300

    Column e Row - modifier do Column

--- app/src/main/java/com/example/checkpoint_1/MainActivity.kt
+++ app/src/main/java/com/example/checkpoint_1/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.background
 import com.example.checkpoint_1.ui.theme.Checkpoint_1Theme
 
 
@@ -31,7 +32,7 @@ class MainActivity : ComponentActivity() {
     }
     @Composable
     fun Inicial(modifier: Modifier = Modifier) {
-        Column {
+        Column(modifier = modifier.background(color=Color.Blue))  {
             TextoCustomizado("Primeira função composable", modifier)
             Row {
                 TextoCustomizado("Seja bem vindo", modifier)
@@ -45,7 +46,7 @@ class MainActivity : ComponentActivity() {
         Text(
             text = texto,
             modifier =  modifier,
-            color = Color.Red
+            color = Color.White
 
 
             )
