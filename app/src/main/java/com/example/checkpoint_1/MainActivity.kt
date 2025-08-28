8c3bd61da2f292ad0502246573c07663c2e4ccdc
Henrique Oliveira <riqueoliveira1910@gmail.com>
2025-08-28 13:00:53 -0300

    Box

--- app/src/main/java/com/example/checkpoint_1/MainActivity.kt
+++ app/src/main/java/com/example/checkpoint_1/MainActivity.kt
@@ -4,17 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.background
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.checkpoint_1.ui.theme.Checkpoint_1Theme
 
 
@@ -25,7 +31,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Checkpoint_1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                   Inicial(Modifier.padding(innerPadding))
+                    NotificationBage(Modifier.padding(innerPadding))
                 }
             }
         }
@@ -51,12 +57,38 @@ class MainActivity : ComponentActivity() {
 
             )
     }
+    @Composable
+    fun NotificationBage(modifier: Modifier = Modifier) {
+        Box(modifier = modifier.padding(16.dp), contentAlignment = Alignment.Center) {
+            Box(
+                modifier = Modifier
+                    .size(24.dp)
+                    .clip(CircleShape)
+                    .background(color = Color.Green)
+            )
+            Box(
+                modifier = Modifier
+                    .size(8.dp)
+                    .clip(CircleShape)
+                    .background(color = Color.Red)
+                    .align(Alignment.TopEnd)
+            )
+        }
+
+    }
 
     @Preview(showBackground = true)
     @Composable
     fun PreviewFuncaoInicial() {
         Checkpoint_1Theme {
             Inicial()
+        }
+    }
+    @Preview(showBackground = true)
+    @Composable
+    fun PreviewNotificationBage() {
+        Checkpoint_1Theme{
+            NotificationBage()
         }
     }
 }
